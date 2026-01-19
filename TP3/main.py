e806e07fdbee61c2f23c5a3888066b08b697a92b
sayansuos <sayan.suos@eleve.ensai.fr>
2026-01-19 18:20:08 +0100

    modif tp3 output

--- TP3/main.py
+++ TP3/main.py
@@ -6,56 +6,48 @@ from TP3.config import INPUT_PATH
 
 def main(queries: list[str], input_path: str, weights: dict) -> dict:
     """
-    Execute the search engine pipeline for a list of queries and display results.
+    Execute the search engine pipeline for a list of queries and save the results in a dict.
 
     :param queries: List of query strings to be processed.
     :type queries: list[str]
     :param input_path: Path to the JSONL file containing the product catalog.
     :type input_path: str
-    :param weights: Dictionary of scoring weights for fields and boosts.
-                    Defaults to predefined values in calculate_linear_scoring.
+    :param weights: Dictionary of scoring weights for fields and boosts. Defaults to predefined values in config.py
     :type weights: dict
-    :return: Dictionary where keys are queries and values are lists of [url, score] pairs for the top 5 results.
+    :return: Dictionary where keys are queries and values are dict with the url, title, description and score for the top 5 results.
     :rtype: dict
     """
-    print("=" * 60)
-    print("Starting test...")
-    print("=" * 60)
 
     df = load_json_as_df(input_path, lines=True)
+    total_docs = len(df)
     responses = {}
     for q in queries:
-        print(f"\nQuery : '{q}'")
-        print(f"-" * 40 + "\n")
+
         results = calculate_linear_scoring(query=q, weights=weights, df=df)
         top5 = list(results.items())[:5]
 
         if not top5 or top5[0][1] == 0:
-            print("No result.")
             continue
 
         list_urls = []
+        responses[q] = {}
+        filtered_docs = sum(1 for score in results.values() if score > 0)
+        responses[q]["metadata"] = {}
+        responses[q]["metadata"]["total_documents"] = total_docs
+        responses[q]["metadata"]["filtered_documents"] = filtered_docs
+
         for i, (url, score) in enumerate(top5):
             list_urls.append([url, score])
             product = df[df["url"] == url].iloc[0]
             title = product["title"]
-            mean_mark = get_reviews(url)["mean_mark"]
-            brand = product.get("product_features", "No result.").get(
-                "brand", "No result."
-            )
-            origin = product.get("product_features", "No result.").get(
-                "made in", "No result."
-            )
+            description = product["description"]
 
-            print(f"{i+1}. [ {title} ]")
-            print(f"   Score: {score:.4f}")
-            print(f"   Mean mark: {mean_mark:.2f}/5")
-            print(f"   Brand: {brand}")
-            print(f"   Origin: {origin}")
-            print(f"   Link: {url}")
-            print()
-
-        responses[q] = list_urls
+            responses[q][str(i + 1)] = {
+                "title": title,
+                "url": url,
+                "description": description,
+                "score": score,
+            }
 
     return responses
 
